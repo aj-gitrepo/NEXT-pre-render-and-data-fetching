dca29e21fb85e398f3c5007238e1fac28bee4792
wink-aajlak <aajlak.wink@gmail.com>
2022-06-21 13:58:34 +0530

    getStaticProps and getStaticPaths

--- notes.js
+++ notes.js
@@ -1,9 +1,25 @@
 // using NEXT js to creae pre generated pages
-// here there the data is loaded before the page is rendered unlike traditional reaat app where the page is hydrated after the page is rendered
+// here there the data is loaded before the page is rendered unlike traditional reaat app where the page is hydrated after the page is rendered (the root div tag is empty and then populated)
 
 // Static generation - compiles the page data on production and cached by the server
 // export async function getStaticProps(contents) {}
 
 // you can run any code that would normally run on the server side only. So, in that function, you don't run the client side code, you're not restricted to that and you don't have access to certain client side API, you don't have access to the window object, for example, but instead, you can run any code you want, that normally would only run on the server side. And even better than that, code that you write inside of this getStaticProps function, will not be included in the code bundle that's sent back to your clients.
 
+// can also specify which page should be pre rendered
+
+// Next.js has a built in feature, which is called incremental static generation. It means that you don't just generate your page statically once at build time, but that it's continuously updated even after deployment without you re-deploying it. 
+
+// So you pre-generate a page, but then you can also tell Next.js that a given page should be re-generated again for every incoming request at most every X seconds. So every 60 seconds, for example. That means that if a request is made for a certain page and it's, let's say less than 60 seconds since it was last re-generated, the existing page would be served to the visitor. 
+
+// But if it's past those 60 seconds and the amount of seconds of course is up to you, then this page would be pre-generated on the server instead. So that means that you either serve the old page if it's not that old yet, or you serve the latest page and brand new page, which was generated again on the server 
+
+// the above is achieved by using revalidate option
+
+// >yarn run build
+
+// 
+
 // server-side - fetches data on request each time before rendereing page
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,64 @@
-const HomePage = () => {
+import fs from 'fs/promises'; //to execute promises //does not work with client side
+import Link from 'next/link';
+import path from 'path';
+
+const HomePage = (props) => {
+  const { products } = props;
   return (
     <div>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {products.map((product) => (
+          <li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>
+        ))}
       </ul>
     </div>
   );
 }
 
+// the props is got first and then the component is rendered
+export async function getStaticProps(context) { //NEXT js looks for this function
+  console.log("Re-generating...")
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if(!data) {
+    return{
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  };
+
+  if(data.products.length === 0) {
+    return {
+      notFound: true //to enable 404 page
+    };
+  }
+  // returns props in the form of object
+  return {
+    props: {
+      products: data.products
+    },
+    revalidate: 10,
+  };
+}
+
 export default HomePage;
+
+// 
+
+// we don't want to load it through an HTTP request. That's only sent from the client site after the page was loaded. Instead we wanna prefetch the data before we create this component. And before this component page gets pre rendered by next JS
+
+// import fs from 'fs';
+// Next JS will ignore it for the client site, it will split your code in a clever way. So to component code in general will be part of the client site code, this import and this code won't be. And now we can use Fs to get access to this dummy backend json file with the read file
+
+// cwd - current working directory
+// when the code is executed by NEXT js it will basically treat all the files as if they sitin our root project folder.So the current working directory will be the overall project folder instead of the pages folder.
+
+// here the dummy-data file is not imported (import creates HTTP request)
+
+// yarn run build
+// pre generates the static pages for production
+
+// revalidate: 10 here, we would tell Next.js that for every incoming request to this page, it should be re-generated unless, it's less than 10 seconds ago that it was last re-generated. So it's recreated at most once every 10 seconds.
